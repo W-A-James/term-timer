8977e3b5a16a2e3e00bff58c14f2b0f42779ebcc
W-A-James <warren.james.dev@gmail.com>
2024-04-21 12:47:35 -0400

    use typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,15 @@
+// @ts-check
+
 import globals from 'globals';
 import js from '@eslint/js';
+import ts from 'typescript-eslint';
 
 export default [
   js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    ignores: ['./bin/*']
+  },
   {
     files: [
       '.eslintrc.{js,cjs}'
